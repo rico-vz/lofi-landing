75e2f10a2fa73f62a959e30f427a8b2771f0db61
Rico van Zelst <ricovzelst@pm.me>
2023-11-28 17:36:04 +0100

    *: install vue & port old page

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="container">
+        <div class="left-container">
+            <div class="gif">
+                <img src="@/assets/lofi.jpg" />
+            </div>
+        </div>
+
+        <div class="right-container">
+            <div class="head">
+                <p>> hi there<span class="blinking">_</span></p>
+            </div>
+
+            <div class="bookmarks">
+                <div class="category">
+                    <div class="links">
+                        <li class="title">1</li>
+                        <li><a href="https://proton.me">proton</a></li>
+                        <li><a href="https://connectme.horizoncollege.nl/">connectme</a></li>
+                        <li><a href="https://trello.com">trello</a></li>
+                        <li><a href="https://purmerend.jarvis.bit-academy.nl/a/dashboard">jarvis</a></li>
+                        <li><a href="https://chat.jarvis.bit-academy.nl/">mattermost</a></li>
+                        <li><a href="https://teams.microsoft.com/_?lm=deeplink&lmsrc=NeutralHomePageWeb&cmpid=WebSignIn&culture=en-us&country=us#/apps/66aeee93-507d-479a-a3ef-8f494af43945/sections/classroom">teams</a></li>
+                    </div>
+                </div>
+
+                <div class="category">
+                    <div class="links">
+                        <li class="title">2</li>
+                        <li><a href="https://github.com/">github</a></li>
+                        <li><a href="https://laracasts.com/">laracasts</a></li>
+                        <li><a href="https://laravel.com/docs/10.x">laravel</a></li>
+                        <li><a href="https://devdocs.io/">devdocs</a></li>
+                        <li><a href="https://ploi.io/">ploi</a></li>
+                    </div>
+                </div>
+
+                <div class="category">
+                    <div class="links">
+                        <li class="title">3</li>
+                        <li><a href="https://www.youtube.com/">youtube</a></li>
+                        <li><a href="https://cinna.triton.usbx.me/jellyfin/">jellyfin</a></li>
+                        <li><a href="https://twitch.tv/">twitch</a></li>
+                        <li><a href="https://www.buienalarm.nl/">buienalarm</a></li>
+                    </div>
+                </div>
+
+                <div class="category">
+                    <div class="links">
+                        <li class="title">4</li>
+                        <li><a href="https://chat.openai.com/">chatgpt</a></li>
+                        <li><a href="https://www.phind.com/">phind</a></li>
+                        <li><a href="https://pomofocus.io/">pomodoro</a></li>
+                        <li><a href="https://readme.so/editor">readme.so</a></li>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
