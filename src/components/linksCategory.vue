57fa8e45fde496f77a53ffb3ef8ac5ba8bdd33e2
Rico van Zelst <ricovzelst@pm.me>
2023-11-28 19:17:03 +0100

    v1: working

---
+++ src/components/linksCategory.vue
@@ -0,0 +1,30 @@
+<template>
+    <template v-for="(value, key) in JSON.parse(links)" :key="key">
+        <div class="category">
+            <div class="links">
+                <li class="title">{{ key }}</li>
+                <ul>
+                    <li v-for="(value, key) in value" :key="key">
+                        <a :href="value.url" target="_blank">{{ value.label }}</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </template>
+</template>
+
+<script>
+
+export default {
+    props: {
+        title: {
+            type: String,
+            required: true
+        },
+        links: {
+            type: String,
+            required: true
+        }
+    }
+};
+</script>
