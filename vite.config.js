d05afbe48dd9c0eb60d97d842d63f988e47b7e90
Rico van Zelst <ricovzelst@pm.me>
2023-12-01 18:22:17 +0100

    aaa

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    minify: "terser",
-    terserOptions: {
-
-    }
+    minify: 'terser',
   },
   plugins: [
     vue(),
